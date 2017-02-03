414fbf7b4ca264fc9d9589bc3cf72a75153d4ee0
ShaunApps <shaun.apps@gmail.com>
2017-02-03 07:37:42 -0500

    react-materialize is a little weird

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Footer } from 'react-materialize';
+import '../.././style/counter.css';
 
 
 export default class FOOTER extends Component {
   render() {
     return (
       <Footer className="footer" copyrights="&copy; 2017 Shaun Appelbaum"
-        
+
       >
           <h5 className="white-text">Footer Content</h5>
           <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, NavItem } from 'react-materialize';
+import '../.././style/counter.css';
 
 
 
